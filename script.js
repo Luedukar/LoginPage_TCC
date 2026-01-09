beb7f23e24cdb77188990046f5ff4f70effe0941
Luiz <luedukar@gmail.com>
2026-01-09 20:26:54 -0300

    Limpeza e ajuste para mobile

--- script.js
+++ script.js
@@ -1,70 +1,65 @@
-const container = document.querySelector(".container");
-const teste5 = document.querySelector(".teste5");
-const teste4 = document.querySelector(".teste4");
-const teste3 = document.querySelector(".teste3");
-const teste2 = document.querySelector(".teste2");
-const teste = document.querySelector(".teste");
+const form_cadastro = document.querySelector(".form_cadastro");
+const form_login = document.querySelector(".form_login");
+const painel_right = document.querySelector(".painel_right");
+const painel_left = document.querySelector(".painel_left");
+const alter_box = document.querySelector(".alter_box");
 const RegisterBtn = document.querySelector(".register-btn");
 const LoginBtn = document.querySelector(".login-btn");
 
 RegisterBtn.addEventListener("click", () => {
-  container.classList.add("active");
-
-  teste.classList.add(
+  alter_box.classList.add(
     "sm:before:left-[50%]",
     "before:left-[0]",
-    "before:top-[70%]"
+    "before:top-[76%]"
   );
 
-  teste2.classList.add(
+  painel_left.classList.add(
     "sm:left-[-50%]",
     "duration-[1000ms]",
     "top-[-30%]"
   );
-  teste2.classList.remove("sm:left-[0]", "top-[0]")
+  painel_left.classList.remove("sm:left-[0]", "top-[0]");
 
-  teste3.classList.add(
+  painel_right.classList.add(
     "sm:right-[0]",
     "sm:duration-[1200ms]",
-    "bottom-[0]",
+    "bottom-[-5%]",
     "absolute"
   );
-  teste3.classList.remove("bottom-[-30%]");
+  painel_right.classList.remove("bottom-[-30%]");
 
-  teste4.classList.remove('delay-300')
-  teste4.classList.add("right-[100%]");
+  form_login.classList.remove("delay-300");
+  form_login.classList.add("right-[100%]");
 
-  
-  teste5.classList.add("sm:right-[50%]", "w-full", "bottom-[16%]", "right-[0]");
-  teste5.classList.remove("right-[-100%]");
-  
-  
-  
+  form_cadastro.classList.add(
+    "sm:right-[50%]",
+    "w-full",
+    "bottom-[12%]",
+    "right-[0]"
+  );
+  form_cadastro.classList.remove("right-[-100%]");
 });
 
 LoginBtn.addEventListener("click", () => {
-  container.classList.remove("active");
-
-  teste.classList.remove(
+  alter_box.classList.remove(
     "sm:before:left-[50%]",
     "before:left-[0]",
-    "before:top-[70%]"
+    "before:top-[76%]"
   );
 
-  teste2.classList.remove(
+  painel_left.classList.remove(
     "sm:left-[-50%]",
     "duration-[1000ms]",
     "top-[-30%]"
   );
-  teste2.classList.add("sm:left-[0]", "top-[0]")
+  painel_left.classList.add("sm:left-[0]", "top-[0]");
 
+  painel_right.classList.remove("sm:right-[0]", "absolute", "bottom-[0]");
+  painel_right.classList.add("bottom-[-30%]");
 
-  teste3.classList.remove("sm:right-[0]", "absolute", 'bottom-[0]');
-  teste3.classList.add("bottom-[-30%]");
+  form_login.classList.remove("right-[100%]");
+  form_login.classList.add("delay-300");
 
-  teste4.classList.remove("right-[100%]");
-  teste4.classList.add('delay-300')
-
-  teste5.classList.remove("sm:right-[50%]", "sm:w-full", "right-[0]");
-  teste5.classList.add("right-[-100%]");
+  form_cadastro.classList.remove("sm:right-[50%]", "sm:w-full", "right-[0]");
+  form_cadastro.classList.add("right-[-100%]");
 });
